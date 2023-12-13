6682f558c9c2e127e600ea916a22734112d4dde4
amandakenes <amandakeness@outlook.com>
2023-12-13 14:41:05 -0300

    att descrescimo de valor

--- desafios/d012/desafio12.js
+++ desafios/d012/desafio12.js
@@ -20,14 +20,14 @@ function precoMudou() {
     } else if (precoAntigo > precoAtual) {
       let baixaPreco = precoAtual - precoAntigo;
       let variacaoPorcentagem = parseFloat(
-        ((precoAtual - precoAntigo) / precoAntigo) * 100
+        ((precoAntigo - precoAtual) / precoAntigo) * 100
       );
       document.getElementById("saida").innerHTML = `
         <h2>Analisando os valores informados</h2>
         <p>O produto custava R$ ${precoAntigo.toFixed(2)} e agora 
         custa R$ ${precoAtual.toFixed(2)}.</p>
         <p>Hoje o produto está mais barato.</p>
-        <p>O preço baixou R$ ${baixaPreco} em relação ao preço
+        <p>O preço caiu R$ ${baixaPreco} em relação ao preço
         anterior.</p>
         <p>Uma variação de ${variacaoPorcentagem.toFixed(1)}% para baixo.</p>
 
