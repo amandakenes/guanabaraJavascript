dbe17e5708ab6ef321e9ec2e50941b88567dc461
amandakenes <amandakeness@outlook.com>
2023-12-15 09:36:19 -0300

    add exercicio 18

---
+++ exercícios/ex018/exercicio18.js
@@ -0,0 +1,22 @@
+let num = Math.floor(Math.random() * 100);
+
+function adivinhar() {
+  let palpite = Number(prompt("Qual é o seu palpite?"));
+  if (palpite == num) {
+    document.querySelector("p").innerHTML += `
+        <p><strong>PARABÉNS!</strong> Você acertou! 
+        Eu tinha sorteado o número ${num}!</p>
+        `;
+        document.querySelector('button').style.display = 'none';    
+  } else if (palpite < num) {
+    document.querySelector("p").innerHTML += `
+        <p>Você falou ${palpite}. Meu número é
+        <strong>MAIOR!</strong></p>
+        `;
+  } else if (palpite > num) {
+    document.querySelector('p').innerHTML += `
+        <p>Você falou ${palpite}. Meu número é
+        <strong>MENOR!</strong></p>
+        `
+  };
+};
