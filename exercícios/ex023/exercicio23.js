4613fcfcb3b2b569212f6cad06f2d4bb2d7a6e90
amandakenes <amandakeness@outlook.com>
2023-12-15 10:31:39 -0300

    add exercicio 23

---
+++ exercícios/ex023/exercicio23.js
@@ -0,0 +1,7 @@
+function mostrarPares() {
+  document.querySelector("p").innerHTML += `
+    <h2>Números pares de 1 até 10</h2>
+    <p>2 &#x1F449 4 &#x1F449 6 &#x1F449 
+    8 &#x1F449 10 &#x1F3C1</p>
+    `;
+}
