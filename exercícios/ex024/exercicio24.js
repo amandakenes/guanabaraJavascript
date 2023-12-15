3f6aa5374fece3671d04d281e728a2f077a1a880
amandakenes <amandakeness@outlook.com>
2023-12-15 10:36:51 -0300

    add exercicio 24

---
+++ exercícios/ex024/exercicio24.js
@@ -0,0 +1,8 @@
+function contagemRegressiva() {
+    document.querySelector('p').innerHTML += `
+    <h2>Contagem regressiva de 10 a 1</h2>
+    <p>10 &#x1F449 9 &#x1F449 8 &#x1F449 7 &#x1F449 
+    6 &#x1F449 5 &#x1F449 4 &#x1F449 3 &#x1F449 
+    2 &#x1F449 1 &#x1F3C1</p>
+    `
+}
