b7d5220640fa14b7050ef259a84eab5d149c6783
amandakenes <amandakeness@outlook.com>
2023-12-15 11:56:36 -0300

    att função

--- exercícios/ex021/exercicio21.js
+++ exercícios/ex021/exercicio21.js
@@ -1,8 +1,12 @@
+let contador = 0;
+
 function contar10() {
-    document.querySelector('p').innerHTML += `
-    <h2>Contando de 1 até 10</h2>
-    <p>1 &#x1F449 2 &#x1F449 3 &#x1F449 4 &#x1F449 
-    5 &#x1F449 6 &#x1F449 7 &#x1F449 8 &#x1F449 
-    9 &#x1F449 10 &#x1F3C1</p>
-    `
+    let saida = document.querySelector('p');
+    while (contador <= 10) {
+        saida.innerHTML += `
+        <h2>Contando de 1 até 10</h2>
+        ${contador} &#x1F449
+        `
+    }
+    saida.innerHTML += '&#x1F3C1'
 }
