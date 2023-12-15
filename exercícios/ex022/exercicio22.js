cf2a2ba675a8ab83c4bea8c185a2e572762111e6
amandakenes <amandakeness@outlook.com>
2023-12-15 10:26:55 -0300

    add exercicio 22

---
+++ exercícios/ex022/exercicio22.js
@@ -0,0 +1,11 @@
+function contarMarcarPares() {
+    document.querySelector('p').innerHTML += `
+    <h2>Contando de 1 até 10, marcando os
+    pares</h2>
+    <p>1 &#x1F449 <mark><strong>2</strong></mark> &#x1F449 
+    3 &#x1F449 <mark><strong>4</strong></mark> &#x1F449 
+    5 &#x1F449 <mark><strong>6</strong></mark> &#x1F449 
+    7 &#x1F449 <mark><strong>8</strong></mark> &#x1F449 
+    9 &#x1F449 <mark><strong>10<qstrong></mark> &#x1F3C1</p>
+    `
+}
