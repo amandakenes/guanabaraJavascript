a832ed572a98d5ae7b762cbeb1d22fd1607c7bcb
amandakenes <amandakeness@outlook.com>
2023-12-15 10:19:38 -0300

    add exercicio 21

---
+++ exercícios/ex021/exercicio21.js
@@ -0,0 +1,8 @@
+function contar10() {
+    document.querySelector('p').innerHTML += `
+    <h2>Contando de 1 até 10</h2>
+    <p>1 &#x1F449 2 &#x1F449 3 &#x1F449 4 &#x1F449 
+    5 &#x1F449 6 &#x1F449 7 &#x1F449 8 &#x1F449 
+    9 &#x1F449 10 &#x1F3C1</p>
+    `
+}
